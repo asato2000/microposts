f3fba44632b24c8c407fe0070b1bc42d3d25cdc3
kudo.asato <asatosouta@gmail.com>
2020-08-22 02:23:07 +0000

    user.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,11 @@ Rails.application.routes.draw do
       get :followings
       get :followers
     end
+    
+  
   end  
   resources :microposts,only: [:create,:destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
 
